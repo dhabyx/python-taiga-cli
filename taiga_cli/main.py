9ed50fb6de61c6ff9f337e261fdb77d191d1eb80
Dhaby Xiloj <dhabyx@gmail.com>
2025-01-15 17:37:47 -0600

    Regresando al menú original

--- taiga_cli/main.py
+++ taiga_cli/main.py
@@ -3,7 +3,7 @@ from taiga_cli.commands import config, login, project, sprint, stories
 
 
 def main():
-    parser.add_argument('--version', action='version', version='taiga-cli 1.1')
+    parser.add_argument('--version', action='version', version='taiga-cli 1.0')
     subparsers = parser.add_subparsers(title='Commands', dest='command')
 
     # Comando: config
@@ -16,10 +16,9 @@ def main():
 
     # Comando: projects
     project_parser = subparsers.add_parser('project', help='Manage projects')
-    project_parser.add_argument('ls', help='List user projects')
-    project_parser.add_argument('default', help='Get default project')
-    project_parser.add_argument('set-default', nargs='*', help='Set default project')
+    project_parser.add_argument('subcommand', nargs='*', help='Subcommands for projects')
     project_parser.add_argument('--all', action='store_true', help='List all projects')
+    project_parser.add_argument('--default', action='store_true', help='Show the default configured project')
     project_parser.set_defaults(func=lambda args: project.run([
         *args.subcommand,
         '--all' if args.all else ''
@@ -27,11 +26,8 @@ def main():
 
     # Comando: sprint
     sprint_parser = subparsers.add_parser('sprint', help='Manage milestones (sprint)')
-    sprint_parser.add_argument('ls', help='List user sprints')
-    sprint_parser.add_argument('set-default', nargs='*', help='Set default sprint')
-    sprint_parser.add_argument('default', help='Get default sprint')
-    sprint_parser.add_argument('user-stats', help='Display user stats of sprint')
-    sprint_parser.add_argument('user-stories', help='Display user stories of sprint')
+    sprint_parser.add_argument('subcommand', nargs='*', help='Subcommands for sprints')
+    sprint_parser.set_defaults(func=lambda args: sprint.run(args.subcommand))
     sprint_parser.add_argument('--user', help='Filter sprint by username', default=None)
     sprint_parser.add_argument('--status', choices=['open', 'closed'], help='Filter sprint by status', default=None)
     sprint_parser.add_argument('--sprint', help='Filter sprint by slug', default=None)
@@ -48,59 +44,15 @@ def main():
 
     # Comando: stories
     stories_parser = subparsers.add_parser('stories', help='Manage stories')
-    stories_subparsers = stories_parser.add_subparsers(title='Subcommands', dest='stories_command')
-
-    # Subcomando: stories ls
-    stories_ls_parser = stories_subparsers.add_parser('ls', help='List user stories')
-    stories_ls_parser.add_argument('--user', help='Filter stories by username', default=None)
-    stories_ls_parser.add_argument('--status', choices=['open', 'closed'], help='Filter stories by status',
-                                   default=None)
-    stories_ls_parser.add_argument('--sprint', help='Filter stories by sprint slug', default=None)
-    stories_ls_parser.add_argument('--project', help='Filter project by sprint slug', default=None)
-    stories_ls_parser.add_argument('--all-users', action='store_true', help='List stories without filtering by user')
-    stories_ls_parser.add_argument('--all-sprints', action='store_true', help='List stories from all sprints')
-    stories_ls_parser.set_defaults(func=lambda args: stories.run([
-        "ls",
-        f"--user={args.user}" if args.user else "",
-        f"--status={args.status}" if args.status else "",
-        f"--sprint={args.sprint}" if args.sprint else "",
-        f"--project={args.project}" if args.project else "",
-        "--all-users" if args.all_users else "",
-        "--all-sprints" if args.all_sprints else ""
-    ]))
-
-    # Subcomando: stories stats
-    stories_stats_parser = stories_subparsers.add_parser('stats', help='Stats of stories')
-    stories_stats_parser.add_argument('--user', help='Filter stories by username', default=None)
-    stories_stats_parser.add_argument('--status', choices=['open', 'closed'], help='Filter stories by status',
-                                      default=None)
-    stories_stats_parser.add_argument('--sprint', help='Filter stories by sprint slug', default=None)
-    stories_stats_parser.add_argument('--project', help='Filter project by sprint slug', default=None)
-    stories_stats_parser.add_argument('--all-users', action='store_true', help='List stories without filtering by user')
-    stories_stats_parser.add_argument('--all-sprints', action='store_true', help='List stories from all sprints')
-    stories_stats_parser.set_defaults(func=lambda args: stories.run([
-        "stats",
-        f"--user={args.user}" if args.user else "",
-        f"--status={args.status}" if args.status else "",
-        f"--sprint={args.sprint}" if args.sprint else "",
-        f"--project={args.project}" if args.project else "",
-        "--all-users" if args.all_users else "",
-        "--all-sprints" if args.all_sprints else ""
-    ]))
-
-    # Subcomando: stories stats-detailed
-    stories_stats_detailed_parser = stories_subparsers.add_parser('stats-detailed', help='Stats and stories of stories')
-    stories_stats_detailed_parser.add_argument('--user', help='Filter stories by username', default=None)
-    stories_stats_detailed_parser.add_argument('--status', choices=['open', 'closed'], help='Filter stories by status',
-                                               default=None)
-    stories_stats_detailed_parser.add_argument('--sprint', help='Filter stories by sprint slug', default=None)
-    stories_stats_detailed_parser.add_argument('--project', help='Filter project by sprint slug', default=None)
-    stories_stats_detailed_parser.add_argument('--all-users', action='store_true',
-                                               help='List stories without filtering by user')
-    stories_stats_detailed_parser.add_argument('--all-sprints', action='store_true',
-                                               help='List stories from all sprints')
-    stories_stats_detailed_parser.set_defaults(func=lambda args: stories.run([
-        "stats-detailed",
+    stories_parser.add_argument('subcommand', nargs='*', help='Subcommands for stories')
+    stories_parser.add_argument('--user', help='Filter stories by username', default=None)
+    stories_parser.add_argument('--status', choices=['open', 'closed'], help='Filter stories by status', default=None)
+    stories_parser.add_argument('--sprint', help='Filter stories by sprint slug', default=None)
+    stories_parser.add_argument('--project', help='Filter project by sprint slug', default=None)
+    stories_parser.add_argument('--all-users', action='store_true', help='List stories without filtering by user')
+    stories_parser.add_argument('--all-sprints', action='store_true', help='List stories from all sprints')
+    stories_parser.set_defaults(func=lambda args: stories.run([
+        *args.subcommand,
         f"--user={args.user}" if args.user else "",
         f"--status={args.status}" if args.status else "",
         f"--sprint={args.sprint}" if args.sprint else "",
